d407d797665b3ebdc3825abed0124a8014be4c67
manfred-commits <manfredipiraino@gmail.com>
2021-09-09 15:24:42 +0200

    aggiunto array di 10 bici

--- snack-1/js/script.js
+++ snack-1/js/script.js
@@ -2,3 +2,52 @@
 // Creare un array di oggetti:
 // Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
 // Stampare a schermo la bici con peso minore utilizzando destructuring e template literal
+
+const bici =[
+    {
+        "nome": "cannondale",
+        "peso": 4,
+    },
+    {
+        "nome": "pinnarello",
+        "peso": 3,
+    },
+    {
+        "nome": "scott",
+        "peso": 6,
+    },
+    {
+        "nome": "canyon",
+        "peso": 5,
+    },
+    {
+        "nome": "vektor",
+        "peso": 11,
+    },
+    {
+        "nome": "sava",
+        "peso": 10,
+    },
+    {
+        "nome": "daricello",
+        "peso": 12,
+    },
+    {
+        "nome": "ciocc",
+        "peso": 9,
+    },
+    {
+        "nome": "bianchi",
+        "peso": 6,
+    },
+    {
+        "nome": "trek",
+        "peso": 8,
+    },
+    {
+        "nome": "colnago",
+        "peso": 4
+    },
+]
+
+
