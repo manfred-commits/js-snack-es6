fc8b524c05d8cb467e2f611ca104ed7951871c92
manfred-commits <manfredipiraino@gmail.com>
2021-09-10 16:02:25 +0200

    project start

---
+++ snack-3/js/script.js
@@ -0,0 +1,30 @@
+
+// // Snack 3
+// Scrivere una funzione che accetti tre argomenti, un array e due numeri (a più piccolo di b).
+// La funzione ritornerà un nuovo array con i valori che hanno la posizione compresa tra i due numeri.
+// Usiamo i nuovi metodi degli array foreach o filter.
+
+
+
+
+
+// SEZIONE CODICE PRINCIPALE
+
+
+
+
+
+// SEZIONE VARIABILI
+
+
+
+
+// /SEZIONE VARIABILI
+
+
+
+
+
+
+// /SEZIONE CODICE PRINCIPALE
+
