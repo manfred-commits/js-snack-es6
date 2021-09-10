8b543c85298d9a4f3a2cfab1f6c11105d1172ccf
manfred-commits <manfredipiraino@gmail.com>
2021-09-10 17:45:22 +0200

    creato funzione come enunciato "Fine esercizio"

--- snack-3/js/script.js
+++ snack-3/js/script.js
@@ -8,7 +8,40 @@
 
 
 // SEZIONE FUNZIONI
-    const rndmInt=(min,max) => Math.floor(Math.random() * (max - min + 1) + min);
+
+
+// 1.2 Scrivere una funzione che accetti tre argomenti, un array e due numeri (a più piccolo di b).
+// La funzione ritornerà un nuovo array con i valori che hanno la posizione compresa tra i due numeri.
+// Usiamo i nuovi metodi degli array foreach o filter.
+
+// utilizzando il metodo filter verifico che gli index dell'array sono compresi tra i due numeri
+
+
+
+
+const rndmInt=(min,max) => Math.floor(Math.random() * (max - min + 1) + min);
+
+
+function sliceWannaby(num1,num2,array){
+
+    while(num1>num2){
+        num1 = rndmInt(0,array.length);
+    }
+    while(num2<num1){
+        num2 = rndmInt(0,array.length);
+    }
+
+    console.log(`Primo numero ${num1}`);
+    console.log(`Secondo numero ${num2}`);
+
+
+    // FILTER SU UNA RIGA
+    const slicedArray = array.filter((element,index) => index>=num1 && index<=num2 );
+    // /FILTER SU UNA RIGA
+
+    return slicedArray;
+
+}
 // /SEZIONE FUNZIONI
 
 
@@ -23,26 +56,21 @@
 // SEZIONE VARIABILI
 
 
-let array = [1,2,3,4,5,6,7,8,9,10,11,12,13];
-let num1 = rndmInt(0,array.length);
-let num2 = rndmInt(0,array.length);
+let arrayToSlice = [1,2,3,4,5,6,7,8,9,10,11,12,13];
+let primoNum = rndmInt(0,arrayToSlice.length);
+let secondoNum = rndmInt(0,arrayToSlice.length);
 
-while(num1>num2){
-    num1 = rndmInt(0,array.length);
-}
-while(num2<num1){
-    num2 = rndmInt(0,array.length);
-}
  
 
-console.log(`Primo numero ${num1}`);
-console.log(`Secondo numero ${num2}`);
-console.log(`Array ${array}`);
+console.log(`Primo numero ${primoNum}`);
+console.log(`Secondo numero ${secondoNum}`);
+console.log(`Array ${arrayToSlice}`);
 
 
 
 // /SEZIONE VARIABILI
 
+// FILTER
 
 // 1. Scrivere una funzione che accetti tre argomenti, un array e due numeri (a più piccolo di b).
 // La funzione ritornerà un nuovo array con i valori che hanno la posizione compresa tra i due numeri.
@@ -51,24 +79,19 @@ console.log(`Array ${array}`);
 // utilizzando il metodo filter verifico che gli index dell'array sono compresi tra i due numeri
 
 
-
 // const slicedArray = array.filter((element,index) => {
 //     if(index>=num1 && index<=num2){
 //         return true;
 //     }
 // });
 
-// FILTER SU UNA RIGA
+// /FILTER
 
-const slicedArray = array.filter((element,index) => index>=num1 && index<=num2 );
+const arraySliced = sliceWannaby(primoNum,secondoNum,arrayToSlice);
 
-// /FILTER SU UNA RIGA
-
-
-console.log(slicedArray);
 console.log(`Nuovo array generato:
-${slicedArray}
-prendendo gli elementi di indice compresi tra i numeri ${num1} e ${num2}`);
+${arraySliced}
+prendendo gli elementi di indice compresi tra i numeri ${primoNum} e ${secondoNum}`);
 
 
 
