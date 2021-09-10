4d80116d719fb11f7925337e99c7208c570c5a13
manfred-commits <manfredipiraino@gmail.com>
2021-09-10 17:20:31 +0200

    aggiunto variabili base

--- snack-3/js/script.js
+++ snack-3/js/script.js
@@ -7,8 +7,8 @@
 
 
 
-
 // SEZIONE CODICE PRINCIPALE
+
 
 
 
@@ -17,9 +17,24 @@
 // SEZIONE VARIABILI
 
 
+let num1 = 2;
+let num2 = 9 ;
+let array = [1,2,3,4,5,6,7,8,9,10,11,12,13];
+
+console.log(`Primo numero ${num1}`);
+console.log(`Secondo numero ${num2}`);
+console.log(`Array ${array}`);
+
 
 
 // /SEZIONE VARIABILI
+
+
+// 1. Scrivere una funzione che accetti tre argomenti, un array e due numeri (a più piccolo di b).
+// La funzione ritornerà un nuovo array con i valori che hanno la posizione compresa tra i due numeri.
+// Usiamo i nuovi metodi degli array foreach o filter.
+
+// utilizzando il metodo filter verifico che gli index dell'array sono compresi tra i due numeri
 
 
 
